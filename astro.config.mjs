0c385a2208aa5c2f7b5026d764603bf51906149e
vedprakash25 <ved1998karma@gmail.com>
2023-04-23 13:05:44 +0530

    push changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blogs-pot.vercel.app/",
+  site: "https://blogspot.vercel.app/",
   integrations: [tailwind()],
 });
