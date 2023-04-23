435e2b86d26517cc25214d7e7a3764410097fab9
vedprakash25 <ved1998karma@gmail.com>
2023-04-23 12:20:07 +0530

    push changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://vedprakash25.github.io/blogs_pot",
-  // base: "/Blogspot",
+  site: "https://vedprakash25.github.io",
+  base: "/blogs_pot",
   integrations: [tailwind()],
 });
